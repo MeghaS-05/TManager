b746d57ab5561bfb30738c62b9e376290dd66d4f
Megha Sah <168621209+MeghaS-05@users.noreply.github.com>
2026-07-01 19:56:59 +0530

    feat: add UserRepository and TaskRepository interface

---
+++ src/main/java/com/taskmanager/model/Task.java
@@ -0,0 +1,33 @@
+package com.taskmanager.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+//import org.springframework.cglib.core.Local;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+    private LocalDate dueDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
